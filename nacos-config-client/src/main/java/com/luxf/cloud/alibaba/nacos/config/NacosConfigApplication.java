065b1a48fa97f18babccf24c4d25d91586063c9d
卢先锋 <luxf@sunwayworld.com>
2020-08-28 22:52:05 +0800

    关于nacos server集群,持久化的特别说明.

--- nacos-config-client/src/main/java/com/luxf/cloud/alibaba/nacos/config/NacosConfigApplication.java
+++ nacos-config-client/src/main/java/com/luxf/cloud/alibaba/nacos/config/NacosConfigApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+/**
+ * nacos server 默认自带一个derby数据库进行持久化. 正常情况下,集群的时候需要,不能每个nacos server 使用默认的derby,数据不一致.
+ * nacos server支持MySQL数据库。
+ *
+ * nacos持久化详情：官方文档 https://nacos.io/zh-cn/docs/cluster-mode-quick-start.html
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NacosConfigApplication {
