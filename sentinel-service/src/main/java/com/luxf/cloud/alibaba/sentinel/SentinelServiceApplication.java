1f022a99f37bfd97301bcbc6c8b695912d210bc2
卢先锋 <luxf@sunwayworld.com>
2020-08-30 16:40:23 +0800

    sentinel的rule持久化到nacos,redis等,并且可以通过nacos动态配置。

--- sentinel-service/src/main/java/com/luxf/cloud/alibaba/sentinel/SentinelServiceApplication.java
+++ sentinel-service/src/main/java/com/luxf/cloud/alibaba/sentinel/SentinelServiceApplication.java
@@ -1,10 +1,13 @@
 package com.luxf.cloud.alibaba.sentinel;
 
+import com.alibaba.cloud.sentinel.SentinelProperties;
+import com.alibaba.cloud.sentinel.custom.SentinelDataSourceHandler;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
 import com.alibaba.csp.sentinel.property.PropertyListener;
 import com.alibaba.csp.sentinel.slotchain.AbstractLinkedProcessorSlot;
 import com.alibaba.csp.sentinel.slotchain.ProcessorSlotChain;
+import com.alibaba.csp.sentinel.slots.block.AbstractRule;
 import com.alibaba.csp.sentinel.slots.block.authority.AuthorityRule;
 import com.alibaba.csp.sentinel.slots.block.authority.AuthorityRuleManager;
 import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRule;
@@ -62,4 +65,33 @@ public class SentinelServiceApplication {
         SpringApplication.run(SentinelServiceApplication.class, args);
     }
 
+    /**
+     * TODO：关于sentinel的规则如何持久化？
+     * {@link SentinelProperties#datasource}:内部提供了TreeMap类型的datasource属性用于配置数据源信息。
+     * @see SentinelDataSourceHandler#afterSingletonsInstantiated() 初始化DataSource.
+     * @see com.alibaba.cloud.sentinel.custom.SentinelAutoConfiguration  有关sentinel的自动配置.
+     *
+     * 第一步：在yml文件先配置以下属性,ds2可以随意命名.
+     *  spring.cloud.sentinel.datasource.ds2.nacos.server-addr=localhost:8848
+     *  spring.cloud.sentinel.datasource.ds2.nacos.data-id=sentinel-service-flow-rule-nacos-ds
+     *  spring.cloud.sentinel.datasource.ds2.nacos.group-id=DEFAULT_GROUP
+     *  spring.cloud.sentinel.datasource.ds2.nacos.data-type=json
+     *  spring.cloud.sentinel.datasource.ds2.nacos.rule-type=flow
+     *
+     * 第二步：在nacos配置页面新增配置,新增配置时,DataId和Group对应yml配置的属性.配置格式选择json.
+     * TODO：是数组,可以针对多个resource进行持久化.
+     * TODO：为什么要配置这些参数？ 针对不同的Rule,配置的属性不同。具体属性详解{@link AbstractRule,FlowRule,DegradeRule,ParamFlowRule,SystemRule,AuthorityRule}
+     * [
+     * 	   // FlowRule配置.
+     * 	   {
+     *         "resource": "/flow-limit/a", # 资源名称
+     *         "limitApp": "default", # 除了AuthorityRule,都是默认的, 在AuthorityRule中,此处是需要认证的service的名称,用','分隔!
+     *         "grade": 1, # 阈值类型,0：线程数,1：QPS
+     *         "count": 1, # 单机阈值数量
+     *         "strategy": 0, # 流控模式, 0：直接,1：关联,2：链路
+     *         "controlBehavior": 0, # 流控效果, 0：快速失败,1：warm up(预热/冷启动),2：排队等待,3：1和2的组合方式
+     *         "clusterMode": false # 是否集群.
+     *     }
+     * ]
+     */
 }
