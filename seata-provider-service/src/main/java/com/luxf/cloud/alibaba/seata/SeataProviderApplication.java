53a68e4daa8e626bff3f02305be400e448a2b943
卢先锋 <luxf@sunwayworld.com>
2020-08-31 18:26:53 +0800

    spring cloud alibaba seata的pom.xml和application.yml

---
+++ seata-provider-service/src/main/java/com/luxf/cloud/alibaba/seata/SeataProviderApplication.java
@@ -0,0 +1,13 @@
+package com.luxf.cloud.alibaba.seata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeataProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeataProviderApplication.class, args);
+    }
+
+}
